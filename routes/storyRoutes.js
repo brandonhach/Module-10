b4b4afab84477465990d33ca216b1d22074625cb
brandonhach <specialcambo@gmail.com>
2024-04-17 16:05:07 -0400

    forgot to validate put

--- routes/storyRoutes.js
+++ routes/storyRoutes.js
@@ -10,7 +10,7 @@ router.get('/new', isLoggedIn, controller.new);
 router.post('/', isLoggedIn, validateStory, controller.create);
 router.get('/:id', validateId, controller.show);
 router.get('/:id/edit', validateId, isLoggedIn, isAuthor, controller.edit);
-router.put('/:id', validateId, isLoggedIn, isAuthor, controller.update);
+router.put('/:id', validateId, isLoggedIn, isAuthor, validateStory, controller.update);
 router.delete('/:id', validateId, isLoggedIn, isAuthor, controller.delete);
 
 module.exports = router;

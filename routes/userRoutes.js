8be9ae25ae02cfe81a95bf09b09b7ad099ad9aba
brandonhach <specialcambo@gmail.com>
2024-04-17 14:43:15 -0400

    rate limitor for login (5 attempts)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const controller = require('../controllers/userController');
-const {isGuest, isLoggedIn} = require('../middlewares/auth');
+const { isGuest, isLoggedIn } = require('../middlewares/auth');
+const { logInLimiter } = require('../middlewares/rateLimiter');
 
 const router = express.Router();
 
 router.get('/new', isGuest, controller.new);
 router.post('/', isGuest, controller.create);
 router.get('/login', isGuest, controller.getUserLogin);
-router.post('/login', isGuest, controller.login);
+router.post('/login', isGuest, logInLimiter, controller.login);
 router.get('/profile', isLoggedIn, controller.profile);
 router.get('/logout', isLoggedIn, controller.logout);
 

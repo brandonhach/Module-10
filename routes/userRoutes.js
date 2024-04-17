d7ab1004017dfd53011974d9e45eab5074b09524
brandonhach <specialcambo@gmail.com>
2024-04-17 15:49:43 -0400

    expres validation to prevent xss

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,13 +2,26 @@ const express = require('express');
 const controller = require('../controllers/userController');
 const { isGuest, isLoggedIn } = require('../middlewares/auth');
 const { logInLimiter } = require('../middlewares/rateLimiter');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
 router.get('/new', isGuest, controller.new);
 router.post('/', isGuest, controller.create);
 router.get('/login', isGuest, controller.getUserLogin);
-router.post('/login', isGuest, logInLimiter, controller.login);
+router.post(
+	'/login',
+	isGuest,
+	logInLimiter,
+	[
+		body('email', 'Email must be a valid email address').isEmail().trim().escape().normalizeEmail(),
+		body('password', 'Password must be at least 8 characters and at most 64 characters').isLength({
+			min: 8,
+			max: 64,
+		}),
+	],
+	controller.login
+);
 router.get('/profile', isLoggedIn, controller.profile);
 router.get('/logout', isLoggedIn, controller.logout);
 

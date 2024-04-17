82bf7b1d0a42717e981b7dd3185f599405ad9163
brandonhach <specialcambo@gmail.com>
2024-04-17 16:02:42 -0400

    valdiate create story

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,10 +1,29 @@
-exports.validateId = (req, res, next)=>{
-    let id = req.params.id;
-    if(!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid story id');
-        err.status = 400;
-        return next(err);
-    } else {
-        return next();
-    }
+const { body, validationResult } = require('express-validator');
+
+exports.validateId = (req, res, next) => {
+	let id = req.params.id;
+	if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+		let err = new Error('Invalid story id');
+		err.status = 400;
+		return next(err);
+	} else {
+		return next();
+	}
 };
+
+exports.validateStory = [
+	body('title', 'Title cannot be empty').trim().escape().notEmpty(),
+
+	body('content', 'Content must be at least 10 characters long').trim().escape().isLength({ min: 10 }),
+
+	(req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			errors.array().forEach((error) => {
+				req.flash('error', error.msg);
+			});
+			return res.redirect('back');
+		}
+		next();
+	},
+];
